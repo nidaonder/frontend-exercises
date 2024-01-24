a91fa39d47d489223c8af520e385bc1b5098a52e
nidaonder <nidaaonder@gmail.com>
2024-01-24 23:09:40 +0300

    added object.

---
+++ week20/object/object.js
@@ -0,0 +1,9 @@
+let laptop1 = {  // -> suslu parantezler object oldugunu gosterir
+    brand : "Apple",
+    model : "MacBook Pro",
+    "2kg" : 2
+}
+
+console.log(laptop1);
+console.log(laptop1.brand);
+console.log(laptop1["2kg"]);  // -> rakam gelecekse koseli parantez + tirnak
